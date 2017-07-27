06b0bff79be5d33118fe9d5104b10fe906186f8c
Steel Wagstaff <swagstaff@wisc.edu>
2017-07-27 13:01:18 -0500

    Improved commenting
    Improved commenting

--- unizin-allow-custom-post-types.php
+++ unizin-allow-custom-post-types.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * Plugin Name: Unizin Allow Custom Post Types
+ * Plugin URI: https://github.com/SteelWagstaff/unizin-allow-custom-post-types
  * Version: 1.0
  * Description: This plugin allows additional custom post types in Pressbooks.
- * Authors:  Steel Wagstaff <swagstaff@wisc.edu>
+ * Author:  Steel Wagstaff <swagstaff@wisc.edu>
  * License: GPLv2 (or any later version)
  */
 
 /**
- * Allows additional custom post types. In this case, 'glossary'--defined by the CM Tooltip Glossary plugin: https://wordpress.org/plugins/enhanced-tooltipglossary/).
+ * Allows additional custom post types in Pressbooks. Per: https://github.com/pressbooks/pressbooks/blob/4.0.0/inc/posttype/namespace.php#L16-L29 as of 2017-7-12
+ * In this case, 'glossary'--defined by the CM Tooltip Glossary plugin: https://wordpress.org/plugins/enhanced-tooltipglossary/).
  * To add additional types, just add them to the array before or after 'glossary'.
- * Per: https://github.com/pressbooks/pressbooks/blob/4.0.0/inc/posttype/namespace.php#L16-L29 as of 2017-7-12
  */
 
 function unizin_add_post_types ( $post_types) {
