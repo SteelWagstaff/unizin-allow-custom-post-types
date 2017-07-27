393740d46f613b72c02075190af1c65b31ea8c13
Steel Wagstaff <swagstaff@wisc.edu>
2017-07-27 12:07:22 -0500

    Create AllowCustomPostTypes.php

---
+++ AllowCustomPostTypes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Allow Custom Post Types
+ * Version: 1.0
+ * Description: This plugin allows additional custom post types in Pressbooks.
+ * Authors:  Steel Wagstaff <swagstaff@wisc.edu>
+ * License: GPLv2 (or any later version)
+ */
+
+/**
+ * Allows additional custom post types. In this case, 'glossary'--defined by the CM Tooltip Glossary plugin: https://wordpress.org/plugins/enhanced-tooltipglossary/). 
+ * To add additional types, just add them to the array before or after 'glossary'. 
+ * Per: https://github.com/pressbooks/pressbooks/blob/4.0.0/inc/posttype/namespace.php#L16-L29 as of 2017-7-12
+ */
+
+function unizin_add_post_types ( $post_types) {
+	return array_merge( $post_types, [ 'glossary' ] );
+}
+
+add_filter( 'pb_supported_post_types', 'unizin_add_post_types' );
+
+?>
